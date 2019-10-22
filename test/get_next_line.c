1b3cc7b3f3648dbe256732996e6f6a778e3fef37
Tisha Alexia <talexia@ga-s6.21-school.ru>
2019-10-22 13:16:21 +0300

    20191022_1316

--- test/get_next_line.c
+++ test/get_next_line.c
@@ -6,21 +6,40 @@
 /*   By: talexia <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 15:25:53 by talexia           #+#    #+#             */
-/*   Updated: 2019/10/15 17:52:42 by talexia          ###   ########.fr       */
+/*   Updated: 2019/10/22 13:15:30 by talexia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	get_next_line(const int fd, char **line)
+t_list	*ft_check_log(int fd, t_list **log)
 {
-	int		i;
-	char	**test;
+	t_list	*tmp;
+	int		id;
 
-	i = fd;
-	test = line;
+	if (!log)
+		return (NULL);
+	tmp = *log;
+	while (log)
+	{
+		id = (int)tmp->content_size == fd;
+		if (id = fd)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	tmp = ft_lstnew("", fd);
+	ft_lstadd(
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static t_list	*log;
+	t_list			*tmp;
 
 	if (fd < 0 || !line)
+		return (-1);
+	tmp = ft_check_log(fd, &log);
+	if (!tmp)
 		return (-1);
 	return (1);
 }
--- test/test_main/main.c
+++ test/test_main/main.c
@@ -6,7 +6,7 @@
 /*   By: talexia <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 15:45:27 by talexia           #+#    #+#             */
-/*   Updated: 2019/10/15 15:20:39 by talexia          ###   ########.fr       */
+/*   Updated: 2019/10/22 11:46:45 by talexia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 
 int	main(int params, char **files)
 {
-	int			i;
-	int	fd;
-	char		*text;
-	char		**line;
+	int		i;
+	int		fd;
+	char	*text;
+	char	**line;
 
 	if (params > 1)
 	{
 		i = 0;
+		text = "111";
+		line = &text;
 		while (++i < params)
 		{
-			text = "no";
-			line = &text;
 			fd = open(files[i], O_RDONLY);
 			printf("%d\n", get_next_line(fd, line));
 			printf("%s\n", *line);
